a0e27c1f277d81971fb48ba68aad6f4cca7d7405
Denner <dennermpb@hotmail.com>
2021-02-09 16:19:31 -0300

    Fase de construção de requisição HTTP, com server node.js, criação do botão delete.

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -13,19 +13,37 @@ export class CourseListComponent implements OnInit {
     
     _filterBy: string;
 
-    constructor(private courseService: CourseService){
-
-    }
+    constructor(private courseService: CourseService){ }
 
     ngOnInit(): void {
-        this._courses = this.courseService.retrieveAll();
-        this.filteredCourses = this._courses;
+        this.retrieveAll();
+    }
+
+    retrieveAll(): void {
+        this.courseService.retrieveAll().subscribe({
+            next: courses => {
+                this._courses = courses;
+                this.filteredCourses = this._courses;
+            },
+            error: err => console.log('Error', err)
+        })
+    }
+
+    deleteById(courseId:number): void {
+        this.courseService.deleteById(courseId).subscribe({
+            next: () => {
+                console.log('Deleted with success');
+                this.retrieveAll();
+            },
+            error: err => console.log('Error', err)
+        })
     }
 
     set filter(value: string){
         this._filterBy = value;
 
-        this.filteredCourses = this._courses.filter((course: Course) => course.name.toLocaleLowerCase().indexOf(this._filterBy.toLocaleLowerCase()) > -1);
+        this.filteredCourses = this._courses.filter((course: Course) => 
+        course.name.toLocaleLowerCase().indexOf(this._filterBy.toLocaleLowerCase()) > -1);
     }
 
     get filter(){
